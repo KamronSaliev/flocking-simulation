ed3decdff5ef55ee86b95cf132af3a9a77694499
Kamron Saliev <kamron.saliev5@gmail.com>
2019-11-19 12:59:21 +0500

    Added scripts for flock, prefab for agent

---
+++ Assets/_Scripts/FlockAgent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class FlockAgent : MonoBehaviour
+{
+    private CircleCollider2D _agentCollider;
+
+    public CircleCollider2D AgentCollider
+    {
+        get => _agentCollider;
+    }
+
+    void Start()
+    {
+        _agentCollider = GetComponent<CircleCollider2D>();
+    }
+
+    public void Move(Vector2 velocity)
+    {
+        transform.position += new Vector3(velocity.x, velocity.y, 0f) * Time.deltaTime;
+    }
+}
---
+++ Assets/_Scripts/FlockBehaviour.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class FlockBehaviour : ScriptableObject
+{
+    public abstract Vector2 CalculateMove(FlockAgent currentAgent, List<Transform> context, Flock flock);
+}
